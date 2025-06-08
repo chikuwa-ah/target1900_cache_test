87d05f2be7adf144f5e9e52be8dd9c65539d13cd
chikuwa-ah <chikuwah21@gmail.com>
2025-06-08 20:47:29 +0900

    わからない

--- registration.js
+++ registration.js
@@ -5,3 +5,15 @@ if ('serviceWorker' in navigator) {
             .catch(err => console.error('Service Worker registration failed:', err));
     });
 }
+
+if (window.matchMedia('(display-mode: standalone)').matches) {
+    document.documentElement.style.width = '100vw';
+    document.body.style.width = '100vw';
+    document.body.style.overflowX = 'hidden';
+
+    // ページの幅が想定外なら修正
+    if (document.documentElement.scrollWidth > window.innerWidth) {
+        document.body.style.transform = `scale(${window.innerWidth / document.documentElement.scrollWidth})`;
+        document.body.style.transformOrigin = 'top left';
+    }
+}
