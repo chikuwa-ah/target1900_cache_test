364675975f6cb4887e684965b728c3033382c51a
chikuwa-ah <chikuwah21@gmail.com>
2025-06-08 20:24:25 +0900

    いみわかんない

--- registration.js
+++ registration.js
@@ -6,19 +6,18 @@ if ('serviceWorker' in navigator) {
     });
 }
 
-// function fixViewportHeight() {
-//     const vh = window.innerHeight * 0.01;
-//     document.documentElement.style.setProperty('--vh', `${vh}px`);
-// }
-// window.addEventListener('resize', fixViewportHeight);
-// window.addEventListener('load', fixViewportHeight);
+function fixViewportHeight() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+window.addEventListener('resize', fixViewportHeight);
+window.addEventListener('load', fixViewportHeight);
 
 if (window.matchMedia('(display-mode: standalone)').matches) {
     document.documentElement.style.width = '100vw';
     document.body.style.width = '100vw';
     document.body.style.overflowX = 'hidden';
 
-    // ページの幅が想定外なら修正
     if (document.documentElement.scrollWidth > window.innerWidth) {
         document.body.style.transform = `scale(${window.innerWidth / document.documentElement.scrollWidth})`;
         document.body.style.transformOrigin = 'top left';
